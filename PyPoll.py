a3be1f62adcca97e87ee75195ac325d22909a305
Graeme Tank <graeme.tank@gmail.com>
2022-06-28 08:12:09 -0400

    find total vote count

--- PyPoll.py
+++ PyPoll.py
@@ -8,6 +8,9 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# 1. Initialize a total vote counter.
+total_votes = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -21,5 +24,9 @@ with open(file_to_load) as election_data:
 
     # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+        # 2. Add to the total vote count.
+        total_votes += 1
+
+# 3. Print the total votes.
+print(total_votes)
 
