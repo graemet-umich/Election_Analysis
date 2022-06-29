4b075ff495ef2bfe3ebcd24346fd489921c0c607
Graeme Tank <graeme.tank@gmail.com>
2022-06-29 09:18:08 -0400

    deliverable 2 done

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -106,7 +106,7 @@ with open(file_to_save, "w") as txt_file:
         print(county_results, end="")
 
          # 6e: Save the county votes to a text file.
-        #txt_file.write(county_results)
+        txt_file.write(county_results)
 
          # 6f: Write an if statement to determine the winning county and get its vote count.
          # Not so much "winning county" as "county with the greatest turnout"
@@ -122,7 +122,7 @@ with open(file_to_save, "w") as txt_file:
     print(largest_turnout_county_statement)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(largest_turnout_county_statement)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
